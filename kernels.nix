3a02923524abbfb38c6d7f666dbb23ea4f2bff4d
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-12-23 01:04:24 +0800

    fix

--- kernels.nix
+++ kernels.nix
@@ -63,14 +63,14 @@ _: {
           };
           kernelDefconfigs = [
             # separated configs
-            "vendor/kona-perf_defconfig"
-            "vendor/ext_config/moto-kona.config"
-            "vendor/ext_config/pstar-default.config"
-            "vendor/debugfs.config"
+            #"vendor/kona-perf_defconfig"
+            #"vendor/ext_config/moto-kona.config"
+            #"vendor/ext_config/pstar-default.config"
+            #"vendor/debugfs.config"
             # the one which need to be generated before build
-            "lineageos_pstar_defconfig"
+            #"lineageos_pstar_defconfig"
             # the one which extract from a real device
-            #"lineageos_pstar_stock_defconfig"
+            "lineageos_pstar_stock_defconfig"
           ];
           kernelImageName = "Image";
           kernelMakeFlags = [
