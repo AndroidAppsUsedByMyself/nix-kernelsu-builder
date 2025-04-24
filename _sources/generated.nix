76b998db9056b234dfffa9a9ee810a99447e2a70
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-04-24 09:07:37 +0000

    auto: update packages

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -288,17 +288,17 @@
   };
   susfs-android13-5_15 = {
     pname = "susfs-android13-5_15";
-    version = "c4acac23118065f1c194a4f54274c362d4a50a39";
+    version = "b54390da29cb9d414bbc4c58032c62d146b89990";
     src = fetchgit {
       url = "https://gitlab.com/simonpunk/susfs4ksu.git";
-      rev = "c4acac23118065f1c194a4f54274c362d4a50a39";
+      rev = "b54390da29cb9d414bbc4c58032c62d146b89990";
       fetchSubmodules = false;
       deepClone = false;
       leaveDotGit = false;
       sparseCheckout = [ ];
-      sha256 = "sha256-29XIe7L5EwQc2Jg5zdsJDG5e/ILFoHILxGxoNHAN9SU=";
+      sha256 = "sha256-wTsyiHsLax4rNk9zJCndJbEXLhYGUpgjxbOHeSVfuwE=";
     };
-    date = "2025-04-22";
+    date = "2025-04-24";
   };
   wildplus-kernel-patches = {
     pname = "wildplus-kernel-patches";
